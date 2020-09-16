94c527789fcd0e951faba5e8660f34c3bdd02834
iante <31571006+iante@users.noreply.github.com>
2020-09-16 16:32:03 +0300

    Update HomeScreen.js

--- fronte/src/screens/HomeScreen.js
+++ fronte/src/screens/HomeScreen.js
@@ -11,7 +11,7 @@ function HomeScreen(props) {
   const [sortOrder, setSortOrder] = useState('');
   const category = props.match.params.id ? props.match.params.id : '';
   const productList = useSelector(state => state.productList);
-  const {products, loading, error} = productList; /* we are obtaining those values from productList */
+  const {products, loading, error} = productList; /* from store; react-redux hooks*/
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(listProducts(category));
