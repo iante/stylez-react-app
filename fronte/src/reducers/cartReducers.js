1f28420700c7df60c35d6247895aa906cb59952b
iante <31571006+iante@users.noreply.github.com>
2020-09-16 16:40:39 +0300

    Update cartReducers.js

--- fronte/src/reducers/cartReducers.js
+++ fronte/src/reducers/cartReducers.js
@@ -11,13 +11,12 @@ switch(action.type){
         const item = action.payload;
         /* searching for the product in state.CartItems
         
-        (x=> x.product===item.product); --> if product matches,replace the product in the if statetement 
+        (x=> x.product===item.product); --> if product matches,replace the product
         with the products that come to the item */
         const product = state.cartItems.find(x => x.product === item.product);
 
         if(product){ 
-            /*if you add a product in the cart that was already there, the code below will return the new qty
-            and update current items */
+            
            return { cartItems: 
             state.cartItems.map(x=> x.product===product.product ? item : x)};
         }
@@ -25,8 +24,7 @@ switch(action.type){
         return{cartItems: [...state.cartItems, item]};
 
         case CART_REMOVE_ITEM:
-            /* defines actions on removing an item from the cart when user clicks the remove button.
-            i.e filters it out.*/
+           /* Filtering items*/ 
             return {cartItems: state.cartItems.filter(x => x.product !== action.payload)};
 
             case CART_SAVE_SHIPPING:
